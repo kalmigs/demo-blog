7043cc91fbbdc9a5aac2128044279050c1b8c567
Khalil Migallos <kmigallos@gmail.com>
2022-04-11 02:03:03 +0800

    Remove home component;

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <Home />
+  <div>
+    <div class="h-screen bg">
+      <div class="h-20 px-3 flex items-center">
+        <img
+          alt="alt message"
+          src="~assets/images/logo-white.png"
+          height="70"
+          width="70"
+          class="mr-auto"
+        />
+        <NuxtLink class="text-white mr-8" to="/journal">Journal</NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,3 +22,10 @@ export default Vue.extend({
   name: 'IndexPage',
 })
 </script>
+
+<style scoped>
+.bg {
+  background-image: url('https://www.outdooradrentures.com/_nuxt/img/home.c8bff47.jpg');
+  background-size: cover;
+}
+</style>
